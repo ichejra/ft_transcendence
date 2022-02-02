e6000587f7d9fb18203668d814a2b9eea417954d
a-saadi <anouarsaadi1802@gmail.com>
2022-02-02 15:15:14 +0100

    feat: Generate the NestJS app and Add Docker-compose config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port: number = 3000;
-  await app.listen(port, () => console.log(`Server: listening on: http://localhost:${port}`));
+  await app.listen(3000);
 }
 bootstrap();
