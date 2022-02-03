8122c69724cdec588a995fce2d2054dffe39378c
elahyani <hassanlhyani7@gmail.com>
2022-02-03 15:34:27 +0100

    clean main

--- backend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const port: number = 3000;
-  await app.listen(port, () => console.log(`Server: listening on: http://localhost:${port}`));
-}
-bootstrap();
