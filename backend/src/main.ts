638d1830db6b9dbe9d237279ea2c206b54808b0f
a-saadi <anouarsaadi1802@gmail.com>
2022-02-19 16:57:15 +0100

    feat: enable cors for access the server from another domain

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,PATCH,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000, () => {
     console.log(`Server listening on http://localhost:${process.env.PORT}...`);
