a72e9991a522d35202b97ae7702c3d293661c8e0
elahyani <hassanlhyani7@gmail.com>
2022-03-19 13:07:08 +0100

    update footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,13 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
   return (
-    <div className='p-6 bg-black bg-opacity-75 flex items-center justify-center text-white md:text-base text-sm'>
-      <h3 className='font-mono opacity-70 '>
+    <div className="p-4 flex items-center justify-center user-card-bg text-gray-500 md:text-base text-sm">
+      <h3 className="about-family text-sm">
         Copyright &copy; {new Date().getFullYear()}. All rights reserved
       </h3>
-      <div className='cursor-pointer text-xl text-yellow-500 font-bold ml-4'>
-        <Link to='/'>LOGO</Link>
+      <div className="cursor-pointer text-xl text-yellow-500 font-bold ml-4">
+        <Link to="/">
+          <img src="./images/logo-ft-transcendence.png" className="w-16" />
+        </Link>
       </div>
     </div>
   );
