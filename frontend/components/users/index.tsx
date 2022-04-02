15c9ce2e5774d8bbf4cd4929c18ba3d85066987a
elahyani <hassanlhyani7@gmail.com>
2022-04-02 18:33:28 +0000

    REFACTOR: refactor users page to include all the types of users (users/friends/blockedusers) and fix blocked users profile access

--- frontend/components/users/index.tsx
+++ frontend/components/users/index.tsx
@@ -3,9 +3,14 @@ import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import GlobalUsers from "./globaleUsers";
 
 import { FaUsers, FaUsersSlash } from "react-icons/fa";
+import { HiOutlineUserGroup } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
-const AllUsers: React.FC = () => {
-  const [list, setList] = useState(0);
+interface Props {
+  type: string;
+}
+
+const AllUsers: React.FC<Props> = ({ type }) => {
   const { blockedUsers } = useAppSelector((state) => state.friends);
   const {
     nrusers,
@@ -14,34 +19,54 @@ const AllUsers: React.FC = () => {
   } = useAppSelector((state) => state.user);
 
   return (
-    <div className="page-100 mt-20 flex justify-center text-gray-200 bg-black">
-      <div className="w-full h-full flex 2xl:w-[80rem] about-family">
-        <ul className="user-card-bg border-r border-gray-700 w-[24rem] h-full p-4">
-          <li
-            onClick={() => setList(0)}
-            className="hover:bg-opacity-40 hover:scale-105 hover:bg-blue-300 transition duration-300 cursor-pointer m-1 px-4 py-3 flex items-center rounded-lg"
-          >
-            <FaUsers size="1.5rem" className="mr-3" /> Users
+    <div className="page-100 relative mt-20 flex justify-center text-gray-200 bg-black">
+      <div className="absolute overflow-auto w-full h-full flex 2xl:w-[80rem] about-family">
+        <ul className="border-r border-gray-700 sm:w-[24rem] h-full p-2 sm:p-4">
+          <li className="">
+            <Link to="/users">
+              <div
+                className={`hover:bg-opacity-40 ${
+                  type === "users" && "highlight"
+                } hover:scale-105 hover:bg-blue-300 transition duration-300 cursor-pointer m-1 px-4 py-3 flex items-center rounded-lg`}
+              >
+                <HiOutlineUserGroup size="1.5rem" className="sm:mr-3" />{" "}
+                <span className="hidden sm:block">Users</span>
+              </div>
+            </Link>
           </li>
-          <li
-            onClick={() => setList(1)}
-            className="hover:bg-opacity-40 hover:scale-105 hover:bg-blue-300 transition duration-300 cursor-pointer m-1 px-4 py-3 flex items-center rounded-lg"
-          >
-            <FaUsers size="1.5rem" className="mr-3" /> Friends
+          <li className="">
+            <Link to={`/users/${userID}/friends`}>
+              <div
+                className={`hover:bg-opacity-40 ${
+                  type === "friends" && "highlight"
+                } hover:scale-105 hover:bg-blue-300 transition duration-300 cursor-pointer m-1 px-4 py-3 flex items-center rounded-lg`}
+              >
+                <FaUsers size="1.5rem" className="sm:mr-3" />{" "}
+                <span className="hidden sm:block">Friends</span>
+              </div>
+            </Link>
           </li>
-          <li
-            onClick={() => setList(2)}
-            className="hover:bg-opacity-40 hover:scale-105 hover:bg-blue-300 transition duration-300 cursor-pointer m-1 px-4 py-3 flex items-center rounded-lg"
-          >
-            <FaUsersSlash size="1.5rem" className="mr-3" /> Blocked Users
+          <li className="">
+            <Link to={`/users/${userID}/blocked`}>
+              <div
+                className={`hover:bg-opacity-40 ${
+                  type === "blockedUsers" && "highlight"
+                } hover:scale-105 hover:bg-blue-300 transition duration-300 cursor-pointer m-1 px-4 py-3 flex items-center rounded-lg`}
+              >
+                <FaUsersSlash size="1.5rem" className="sm:mr-3" />{" "}
+                <span className="hidden sm:block">Blocked Users</span>
+              </div>
+            </Link>
           </li>
         </ul>
-        <div className="channels-bar-bg w-full">
-          {list === 0 && (
+        <div className="w-full">
+          {type === "users" && (
             <GlobalUsers users={nrusers.filter((user) => user.id !== userID)} />
           )}
-          {list === 1 && <GlobalUsers users={friends} type="friends" />}
-          {list === 2 && <GlobalUsers users={blockedUsers} type="blocked" />}
+          {type === "friends" && <GlobalUsers users={friends} type="friends" />}
+          {type === "blockedUsers" && (
+            <GlobalUsers users={blockedUsers} type="blocked" />
+          )}
         </div>
       </div>
     </div>
