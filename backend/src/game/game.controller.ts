00f85d354761e5321bb9ba0b1ace3bce347a2b39
AnouarSaadi <anouarsaadi1802@gmail.com>
2022-04-13 14:45:24 +0000

    REFACTOR: fetching the game history

--- backend/src/game/game.controller.ts
+++ backend/src/game/game.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, HttpCode, UseGuards } from "@nestjs/common";
+import {
+    Controller,
+    Get,
+    HttpCode,
+    Param,
+    ParseIntPipe,
+    UseGuards
+} from "@nestjs/common";
 import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 import { ReqUser } from "src/users/decorators/req-user.decorator";
 import { User } from "src/users/entities/user.entity";
@@ -7,12 +14,14 @@ import { GameService } from "./game.service";
 
 @Controller('games')
 export class GameController {
-    constructor(private gameService: GameService) {}
-    
-    @Get()
+    constructor(private gameService: GameService) { }
+
+    @Get('user/:userId')
     @HttpCode(200)
     @UseGuards(JwtAuthGuard)
-    getGamesHistory(@ReqUser() user: User): Promise<Game[]> {
-        return this.gameService.getGamesHistory(Number(user.id));
+    getGamesHistory(
+        @ReqUser() user: User,
+        @Param('userId', ParseIntPipe) userId: number): Promise<Game[]> {
+        return this.gameService.getGamesHistory(Number(userId));
     }
 }
